2541f26d414d3b2b8f9064e8db2779a4b0d4eb5e
adil-82 <bouaddiadil82@gmail.com>
2021-08-04 15:35:03 +0000

    more styling nav

--- components/Header.js
+++ components/Header.js
@@ -21,11 +21,11 @@ function Logo() {
             <a>
                 {/* <span className="bg-white inset-0 transform rounded "> */}
                 {/* </span> */}
-                <div className="relative bg-white pb-5 px-5 transform skew-x-12 rounded-xl">
+                <div className="relative text-center bg-white pb-5 px-2 transform skew-x-12 rounded-xl">
 
 
-            <Image width={100} height={40} className="rounded-lg" src={logo} alt="logo" />
-                <p className="absolute text-xs text-red-700 font-semibold m-0">
+            <Image width={125} height={40} className="rounded-lg" src={logo} alt="logo" />
+                <p className="absolute left-0 right-0 text-xs text-red-900 font-semibold m-0">
             مركز الحجامة للطب البديل
                 </p>
                 </div>
@@ -36,9 +36,9 @@ function Logo() {
 
 function SiteNav() {
     return (
-        <div className="relative  py-5 font-extrabold ">
-            <Link href="/contact"><a className="bg-red-700 hover:bg-red-400 py-2 px-3 rounded-xl no-underline text-white">اتصل بنا</a></Link>
-            <Link href="/qui-somme-nous"><a className="bg-red-700 hover:bg-red-400 py-2 px-3 md:mr-4 m-1 rounded-xl no-underline text-white"> من نحن </a></Link>
+        <div className="relative md:text-lg sm:text-base text-xs py-5 font-extrabold ">
+            <Link href="/contact"><a className="bg-red-700 hover:bg-red-400 py-2 px-2 rounded-xl no-underline text-white">اتصل بنا</a></Link>
+            <Link href="/qui-somme-nous"><a className="bg-red-700 hover:bg-red-400 py-2 px-2 md:mr-4 m-1 rounded-xl no-underline text-white"> من نحن </a></Link>
             {/* <Link href="/"><a className="bg-red-700 hover:bg-red-400 py-2 px-3 md:mr-4 m-1 rounded-xl no-underline text-white">الرئيسية</a></Link> */}
         </div>
     )
